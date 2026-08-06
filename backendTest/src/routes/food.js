6cc9387ef550dd9f9236bd00414414259eab49e8
dullee <91831552+dullee@users.noreply.github.com>
2026-08-06 18:00:44 +0800

    fixed getfood

--- backendTest/src/routes/food.js
+++ backendTest/src/routes/food.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getUser } from "../resolvers/user/get-user.js";
+import { getFood } from "../resolvers/food/get-food.js";
 import { createFood } from "../resolvers/food/create-food.js";
 
 export const foodRouter = express.Router();
 
-foodRouter.get("/", getUser);
-foodRouter.post('/', createFood)
+foodRouter.get("/", getFood);
+foodRouter.post("/", createFood);
