48902c6eea8659769ba351efb334af0419dd7500
dullee <91831552+dullee@users.noreply.github.com>
2026-08-11 20:00:27 +0800

    feat: integrate bcryptjs for password hashing in user creation; update package dependencies

--- backendTest/src/resolvers/user/create-user.js
+++ backendTest/src/resolvers/user/create-user.js
@@ -1,10 +1,14 @@
 import { UserModel } from "../../models/user-model.js";
+import bcrypt from "bcryptjs";
 
 export const createUser = async (req, res) => {
   const body = req.body;
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash;
+  (body.password, saltRounds);
   const newUser = await UserModel.create({
     email: body.email,
-    password: body.password,
+    password: hashedPassword,
     phoneNumber: body.phoneNumber,
     address: body.address,
     role: body.role,
