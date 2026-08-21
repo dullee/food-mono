ff1e0882324c34aa4adf8db1c3aa4c2669285fd1
dullee <91831552+dullee@users.noreply.github.com>
2026-08-21 13:45:24 +0800

    feat: add pagination component to AdminOrderInfo for improved order navigation

--- food-front-folder/app/admin/_components/adminOrderInfo.tsx
+++ food-front-folder/app/admin/_components/adminOrderInfo.tsx
@@ -6,6 +6,16 @@ import { Button } from "@/components/ui/button";
 import { Check, CalendarRange, ChevronDown } from "lucide-react";
 import { ColumnDef } from "@tanstack/react-table";
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
 import { useEffect, useState } from "react";
 import { DataTable } from "./dataTable";
 import {
@@ -15,6 +25,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Order } from "@/app/types/order.js";
+
+
+let pageNumber = 1
 
 function SquareToggleButton() {
   const [checked, setChecked] = useState(false);
@@ -266,6 +279,30 @@ export default function AdminOrderInfo() {
         {error && <p className="text-sm text-red-500 py-4 px-4">{error}</p>}
         {!isLoading && !error && <DataTable columns={columns} data={orders} />}
       </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" isActive>
+              {pageNumber}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }
